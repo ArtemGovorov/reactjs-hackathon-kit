f4eb8b4b415d803562b66c0f85c130dc09a36720
Nathan Vale <hi@nathanvale.com>
2016-08-03 14:52:40 +1000

    wip

--- src/shared/layouts/CoreLayout/CoreLayout.tsx
+++ src/shared/layouts/CoreLayout/CoreLayout.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import SplitReducer from 'shared/components/SplitReducer';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-//import styles from 'styles/main.css';
 const styles = require('styles/main.css');
 const classNames = require('classnames/bind');
 const cx = classNames.bind(styles);
-console.log(styles);
+
 function CoreLayout(props) {
   return (
     <SplitReducer {...props}>
